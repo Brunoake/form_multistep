6b63928a98835101525eff1382ac94147b31da1e
Brunoake <126792752+Brunoake@users.noreply.github.com>
2024-02-22 00:27:18 -0300

    .

---
+++ form_multstep/src/App.jsx
@@ -0,0 +1,32 @@
+// Components
+import {GrFormNext, GrFormPrevious} from 'react-icons/gr'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='app'>
+    <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>Ficamos felizes com a sua compra, utilize o formulario abaixo para avaliar o produto</p>
+      </div> 
+      <div className="form-container">
+        <p>Etapas</p>
+        <form >
+          <div className="inputs-container">
+            
+          </div>
+          <div className="actions">
+            <button type='button'>
+             <span>Voltar</span> <GrFormPrevious/></button>
+            <button type='submit'>
+              <span>Avançar</span><GrFormNext/></button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App
